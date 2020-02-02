aaf173abe1b7d4c3e8774358ddb39a9150fee75a
Shobhit Ratan <shobhit.ratan@baruchmail.cuny.edu>
2020-02-02 00:35:47 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -42,8 +42,10 @@ def update_counting_hash(hash, key)
   # if the provided key is present, increment its value by 1
   hash = {key => value}
   if hash[key]
-    hash[key] += 1
+    value += 1
+    hash[key] = value
   else
-    hash[key] = 1
+    value = 1
+    hash[key] = value 
   end
 end
